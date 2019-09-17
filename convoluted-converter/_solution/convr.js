d79b266ada9ad4f8a78702161bf41ba95a409d93
延 <bsingr@users.noreply.github.com>
2019-09-17 19:25:45 +0200

    Refactor fn, naming, error, regexp

--- convoluted-converter/_solution/convr.js
+++ convoluted-converter/_solution/convr.js
@@ -1,29 +1,28 @@
 const numberSystems = [
-  {name: "bin", prefix: "0b", base: 2, shape: /^0b[01]+$/},
-  {name: "hex", prefix: "0x", base: 16, shape: /^0x[0-9a-fA-F]+$/},
-  {name: "dec", prefix: "", base: 10, shape: /^(0|[1-9]\d*)$/},
+  {name: "bin", prefix: "0b", base: 2, shape: /^0b[01]+$/},
+  {name: "hex", prefix: "0x", base: 16, shape: /^0x[0-9a-f]+$/i},
+  {name: "dec", prefix: "", base: 10, shape: /^(0|[1-9]\d*)$/},
 ];
 
-const normalise = (ns, input) => parseInt(input.substr(ns.prefix.length), ns.base);
-const translate = (ns, intermediate) => ns.prefix + intermediate.toString(ns.base);
-const convert = (targetName, input) => {
-  const inputNS = numberSystems.find(n => n.shape.test(input))
-    || (() => {throw "Input number invalid"})();
-  const targetNS = numberSystems.find(n => n.name === targetName)
-    || (() => {throw "Target option invalid"})();
-  const intermediate = normalise(inputNS, input);
-  return translate(targetNS, intermediate);
+const findNumberSystemByName = name => numberSystems.find(ns => ns.name === name);
+const findNumberSystemByExampleNumber = numberStr => numberSystems.find(ns => ns.shape.test(numberStr));
+const parseNumber = (ns, numberStr) => parseInt(numberStr.substr(ns.prefix.length), ns.base);
+const formatNumber = (ns, number) => ns.prefix + number.toString(ns.base);
+const convert = (targetNumberSystemName, inputNumberStr) => {
+  const inputNS = findNumberSystemByExampleNumber(inputNumberStr) || fail("Input number invalid");
+  const targetNS = findNumberSystemByName(targetNumberSystemName) || fail("Target option invalid");
+  const inputNumber = parseNumber(inputNS, inputNumberStr);
+  return formatNumber(targetNS, inputNumber);
 };
+const fail = message => { throw message };
 
 try {
-  if (process.argv.length !== 4) {
-    throw "Wrong number of arguments";
-  }
-  const targetName = process.argv[2].substr(1);
-  const input = process.argv[3];
-  const result = convert(targetName, input);
-  console.log(result);
+  if (process.argv.length !== 4) fail("Wrong number of arguments");
+  const targetNumberSystemName = process.argv[2].substr(1);
+  const inputNumberStr = process.argv[3];
+  const result = convert(targetNumberSystemName, inputNumberStr);
+  console.log(result);
 } catch (e) {
   console.log("Error: " + e);
-  process.exit(1);
+  process.exit(1);
 }
